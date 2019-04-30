75cfd13f6f81df982b95240e17d7786c50040e16
charlychiu <charlychiu@gmail.com>
2019-04-30 18:14:49 +0800

    Add package brozot/laravel-fcm & testing code

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use LaravelFCM\Facades\FCM;
+
+
+class TestController extends Controller
+{
+
+    /**
+     * Sending a downstream message to a device
+     *
+     */
+    function test()
+    {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60*20);
+
+        $notificationBuilder = new PayloadNotificationBuilder('my title');
+        $notificationBuilder->setBody('Hello world')->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['a_data' => 'my_data']);
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        $token = 'a_registration_from_your_database';
+
+        $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+
+        $downstreamResponse->numberSuccess();
+        $downstreamResponse->numberFaulure();
+        $downstreamResponse->numberModification();
+
+        $downstreamResponse->tokensToDelete();
+
+        $downstreamResponse->tokensToModify();
+
+        $downstreamResponse->tokensToRetry();
+
+    }
+
+}
